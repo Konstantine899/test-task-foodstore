936137d4995049d7a37013e8b65e8059ac741190
unknown <C:\Users\kosta\AppData\Roaming\The Bat!>
2025-09-05 10:42:57 +0300

    build: убрал задержку в deServer

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,21 +6,5 @@ export function buildDevServer(options: BuildOptions): Configuration {
     port: options.port,
     open: true,
     hot: true,
-    setupMiddlewares: (middlewares, devServer) => {
-      // Middleware для задержки всех запросов
-      devServer.app.use('*', (req, res, next) => {
-        const startTime = Date.now();
-        console.log(`[${new Date().toISOString()}] Запрос: ${req.method} ${req.url}`);
-        console.log('Задержка 2 секунд...');
-        
-        setTimeout(() => {
-          const duration = Date.now() - startTime;
-          console.log(`Запрос обработан за ${duration}ms`);
-          next();
-        }, 2000);
-      });
-      
-      return middlewares;
-    }
   };
 }
